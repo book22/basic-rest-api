5fc3fb8263a538423ce68b63c75eaf6641e90d2c
本條　祐一朗 <bookking0122@yahoo.co.jp>
2021-01-10 11:45:58 +0900

    no message

---
+++ basic-rest-apiのコピー/app/app.js
@@ -0,0 +1,162 @@
+const express = require('express')
+const app = express()
+const sqlite3 = require('sqlite3')
+const path = require('path')
+const bodyParser = require('body-parser')
+
+const dbPath = "app/db/database.sqlite3"
+
+//リクエストのbodyをパースする設定
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+//静的ファイル群
+app.use(express.static(path.join(__dirname, 'public')))
+
+// get all users
+app.get('/api/v1/users',(req, res) => {
+  //connect database
+  const db = new sqlite3.Database(dbPath)
+
+  db.all('SELECT * FROM users', (err, rows) => {
+    res.json(rows)
+  })
+
+  db.close()
+})
+
+// get a user
+app.get('/api/v1/users/:id',(req, res) => {
+  //connct database
+  const db = new sqlite3.Database(dbPath)
+  const id = req.params.id
+
+  db.get(`SELECT * FROM users WHERE id = ${id}`, (err, row) => {
+    if(!row){
+      res.status(404).send({error:"not found"})
+    } else {
+      res.status(200).json(row)
+    }
+  })
+
+  db.close()
+})
+
+// search users matching keyword
+app.get('/api/v1/search',(req, res) => {
+  //connect database
+  const db = new sqlite3.Database(dbPath)
+  const keyword = req.query.q
+
+  db.all(`SELECT * FROM users WHERE name LIKE "%${keyword}%"`, (err, rows) => {
+    res.json(rows)
+  })
+
+  db.close()
+})
+
+//runファンクション定義
+
+const run = async (sql, db) => {
+  return new Promise((resolve, reject) => {
+    db.run(sql, (err) => {
+      if(err) {
+        return reject(err)
+      } else {
+        return resolve()
+      }
+    })
+  })
+}
+
+// create a new user
+app.post('/api/v1/users', async (req, res) => {
+  if(!req.body.name ||req.body.name === ""){
+    res.status(400).send({error:"ユーザー名が指定されていません"})
+  } else {
+
+    //connect database
+    const db = new sqlite3.Database(dbPath)
+    
+    const name = req.body.name 
+    const profile = req.body.profile ? req.body.profile : ""
+    const dateOfBirth = req.body.date_of_birth ? req.body.date_of_birth : ""
+    
+    try {
+      await run(
+        `INSERT INTO users (name, profile, date_of_birth) VALUES ("${name}", "${profile}","${dateOfBirth}"
+        )`,
+        db
+      )
+      res.status(201).send({message: "ユーザー作成"})
+    } catch (e) {
+      res.status(500).send({error: e})
+    }
+
+      db.close()
+    }
+  })
+    
+
+// update a new user
+app.put('/api/v1/users/:id', async (req, res) => {
+  if(!req.body.name ||req.body.name === ""){
+    res.status(400).send({error:"ユーザー名が指定されていません"})
+  } else {
+    //connect database
+    const db = new sqlite3.Database(dbPath)
+    const id = req.params.id
+
+    //現在のユーザー情報を取得する
+    db.get(`SELECT * FROM users WHERE id = ${id}`, async (err, row) => {
+
+      if(!row) {
+        res.status(404).send({error: "ユーザーが見つかりません"})
+      } else {
+
+        
+        const name = req.body.name ? req.body.name : row.name
+        const profile = req.body.profile ? req.body.profile : row.profile
+        const dateOfBirth = req.body.date_of_birth ? req.body.date_of_birth : row.date_of_birth
+        
+        try {
+          await run(
+            `UPDATE users SET name="${name}", profile="${profile}", date_of_birth="${dateOfBirth}" WHERE id=${id}`,
+          db)
+          res.status(200).send({message: "ユーザーを更新"})
+        } catch (e) {
+          res.status(500).send({error: e})
+        }
+      }
+    })
+
+    db.close()
+  }
+})
+
+// delete a new user
+app.delete('/api/v1/users/:id', async (req, res) => {
+    //connect database
+    const db = new sqlite3.Database(dbPath)
+    const id = req.params.id
+
+  // 現在のユーザー情報を取得する
+  db.get(`SELECT * FROM users WHERE id=${id}`, async (err, row) => {
+
+    if (!row) {
+      res.status(404).send({error: "指定されたユーザーが見つかりません。"})
+    } else {
+      try {
+        await run(`DELETE FROM users WHERE id=${id}`, db)
+        res.status(200).send({message: "ユーザーを削除しました。"})
+      } catch (e) {
+        res.status(500).send({error: e})
+      }
+    }
+  })
+    db.close()
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port)
+console.log('listen on port:' + port);
---
+++ basic-rest-apiのコピー/app/public/js/index.js
@@ -0,0 +1,51 @@
+const indexModule = (() => {
+  const path = window.location.pathname
+
+  switch(path){
+    case '/':
+      //検索クリック
+      document.getElementById('search-btn')
+      .addEventListener('click', () => {
+        return searchModule.searchUsers()
+      })
+    
+      // Usersモジュール
+      return usersModule.fetchAllUsers()
+
+      case '/create.html': 
+        document.getElementById('save-btn')
+        .addEventListener('click', () => {
+          return usersModule.createUser()
+        })
+        
+        document.getElementById('cancel-btn')
+        .addEventListener('click', () => {
+          return window.location.href = '/'
+        })
+        break;
+
+        case '/edit.html': 
+        const uid = window.location.search.split('?uid=')[1]
+
+        document.getElementById('save-btn')
+        .addEventListener('click', () => {
+          return usersModule.saveUser(uid)
+        })
+        
+        document.getElementById('cancel-btn')
+        .addEventListener('click', () => {
+          return window.location.href = '/'
+        })
+
+        document.getElementById('delete-btn')
+        .addEventListener('click', () => {
+          return usersModule.deleteUser(uid)
+        })
+
+        return usersModule.setExistingValue(uid)
+
+        default:
+          break;
+  }
+
+})()
